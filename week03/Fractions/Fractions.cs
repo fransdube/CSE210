3562399cae5ad032f25cb3885412a8d5cedccf30
fransdube <fransdube@gmail.com>
2025-01-28 12:20:46 +0200

    Fractions

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,64 @@
+public class Fraction
+{
+    // Private attributes
+    private int _top;
+    private int _bottom;
+
+    // Constructors
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Getters and Setters
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        if (bottom != 0) // Ensure denominator is not zero
+        {
+            _bottom = bottom;
+        }
+        else
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+    }
+
+    // Methods to return representations
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
